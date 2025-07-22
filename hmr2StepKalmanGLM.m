56af17b22635fcc504cc59cabc3aea01887c42c8
Jahani-dev <jahanisahar0@gmail.com>
2025-07-21 21:41:37 -0700

    hmr2StepKalmanGLM.m

--- hmr2StepKalmanGLM.m
+++ hmr2StepKalmanGLM.m
@@ -4,7 +4,6 @@ function [ddK1 ddKS1 Pfinal PfinalS] = hmrKalmanGLMAutoINI_2step(d, s, t, SD, th
 % UI NAME: 
 % hmrKalmanGLMAutoINI_2step
 % Kalman_GLM_AutoINI_2step  created 05-2020 by S. Jahani  
-% Kalman_GLM
 % This script estimates the HRF in two steps using Kalman filter and GLM with Automatic
 % intialization for measurement noise
 %
